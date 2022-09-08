25b941644858e1c4888d5712c72ccef37b72b82b
Antanas <Antanas@DESKTOP-E89NA7E>
2022-09-08 14:47:58 +0300

    Initial commit

---
+++ tests/TowerDefense.Api.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TowerDefense.Api.FunctionalTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
