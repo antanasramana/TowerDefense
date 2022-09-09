322449e37482f7b4d7d5ca58c47799e265e9058f
tomvai15 <78442825+tomvai15@users.noreply.github.com>
2022-09-09 15:07:13 +0300

    Added SignalR nuget to api

---
+++ src/TowerDefense.Api/Hubs/GameHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TowerDefense.Api.Hubs
+{
+    public class GameHub :Hub
+    {
+        public async Task HelloServer(string user)
+        {
+            await Clients.All.SendAsync("HelloClient", $"Hello {user}!");
+        }
+    }
+}
