631b069ad0a73adaa2becbf2affa3af102349d90
mizge <ugne.glinskyte@ktu.edu>
2022-10-18 21:23:07 +0300

    Decorator implemented

---
+++ src/TowerDefense.Api/Decorator/DamageDecorator.cs
@@ -0,0 +1,25 @@
+﻿using TowerDefense.Api.Battle.Grid;
+using TowerDefense.Api.Models;
+using TowerDefense.Api.Models.Items;
+
+namespace TowerDefense.Api.Decorator
+{
+    public class DamageDecorator : BaseDecorator
+    {
+        public DamageDecorator(IItem item) : base(item)
+        {
+        }
+
+        public override IEnumerable<AttackDeclaration> Attack(GridItem[] opponentGridItems, int attackingGridItemId)
+        {
+            var attackResult = _item.Attack(opponentGridItems, attackingGridItemId).ToList();
+            attackResult.ForEach(x => IncreaseDamage(x));
+            return attackResult;
+        }
+
+        private static void IncreaseDamage(AttackDeclaration attackDeclaration)
+        {
+            attackDeclaration.Damage *= 2;
+        }
+    }
+}
---
+++ src/TowerDefense.Api/Decorator/MoneyDecorator.cs
@@ -0,0 +1,25 @@
+﻿using TowerDefense.Api.Battle.Grid;
+using TowerDefense.Api.Models.Items;
+using TowerDefense.Api.Models;
+
+namespace TowerDefense.Api.Decorator
+{
+    public class MoneyDecorator : BaseDecorator
+    {
+        public MoneyDecorator(IItem item) : base(item)
+        {
+        }
+
+        public override IEnumerable<AttackDeclaration> Attack(GridItem[] opponentGridItems, int attackingGridItemId)
+        {
+            var attackResult = _item.Attack(opponentGridItems, attackingGridItemId).ToList();
+            attackResult.ForEach(x => CalculateEarnedMoney(x));
+            return attackResult;
+        }
+
+        private static void CalculateEarnedMoney(AttackDeclaration attackResult)
+        {
+            attackResult.EarnedMoney = 10;
+        }
+    }
+}
