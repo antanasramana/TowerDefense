107e3ba3bd4e01c552cc462bbd695d0c231373ef
mizge <ugne.glinskyte@ktu.edu>
2022-11-13 17:41:47 +0200

    trash 3

--- src/TowerDefense.Api/GameLogic/Observer/GridPublisher.cs
+++ src/TowerDefense.Api/GameLogic/Observer/GridPublisher.cs
@@ -4,23 +4,23 @@ namespace TowerDefense.Api.GameLogic.Observer
 {
     public class GridPublisher : IGridPublisher
     {
-        private readonly List<IAttackSubscriber> _subscribers = new List<IAttackSubscriber>();
+        private readonly List<IAttackSubscriber> Subscribers = new List<IAttackSubscriber>();
 
         public void Attach(IAttackSubscriber subscriber)
         {
-            _subscribers.Add(subscriber);
+            Subscribers.Add(subscriber);
         }
 
         public void Detach(IAttackSubscriber subscriber)
         {
-            _subscribers.Remove(subscriber);
+            Subscribers.Remove(subscriber);
         }
 
         public List<AttackResult> Notify(IEnumerable<AttackDeclaration> attackDeclarations)
         {
             List<AttackResult> attackResults = new List<AttackResult>();
 
-            foreach (var subscriber in _subscribers)
+            foreach (var subscriber in Subscribers)
             {
                 foreach (var attackDeclaration in attackDeclarations)
                 {
