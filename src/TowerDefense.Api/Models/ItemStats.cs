a4bff05547673ce4c9d35d73dd8c77a53304c905
Eimantas Mažeika <eima.mazeika@gmail.com>
2022-11-13 13:49:06 +0200

    [ADD] Custom SQ rule

--- src/TowerDefense.Api/Models/ItemStats.cs
+++ src/TowerDefense.Api/Models/ItemStats.cs
@@ -94,7 +94,28 @@
 
         public IItemStats Clone()
         {
-            return new RegularDefaultItemStats { Damage = Damage, Price = Price, Health = Health };
+            return new RegularDefaultItemStats { Damage = Damage, Price = Price, Health = Health }; // This line should trigger new custom rule
         }
+        /*1
+         2
+        3
+        4
+        5
+        6
+        7
+        8
+        9
+        10
+        11
+        12
+        13
+        14
+        15
+        16
+        17
+        18
+        19
+        20
+        21*/
     }
 }
